2533c4848744ac662400857c423ccd26cbe6c8d9
EricKugel <erickugel713@gmail.com>
2024-02-12 22:44:24 -0600

    show all the little users

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,42 @@
 import { useSession } from "next-auth/react";
 
-import Link from "next/link";
+import { connectDB } from "../helpers/mongohelper";
 
-const Home = () => {
+import Link from "next/link";
+import User from "../models/user";
+
+const Home = ({ users }) => {
   const session = useSession();
   return (
     <>
-      <p>Hello, world!</p>
       <div>Current session:</div>
       <div>{JSON.stringify(session?.data)}</div>
       <Link href="/login">Sign me in!</Link>
+      <hr></hr>
+      <div>
+        <div>All current users:</div>
+        <ol>
+          {users.map((user) => <li key={user._id}>{user.email}</li>)}
+        </ol>
+      </div>
     </>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  try {
+    await connectDB();
+    console.log(JSON.parse(JSON.stringify(await User.find({}).lean())));
+    return {
+      props: {
+        users: JSON.parse(
+          JSON.stringify(await User.find({}).lean())
+        ),
+      },
+    };
+  } catch (e) {
+    console.error(e);
+  }
+}
